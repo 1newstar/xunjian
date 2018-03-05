d252361de341f507e93579554b4d154e923212e3
yanggege <yangguocang>
2018-03-05 20:04:29 +0800

    11

--- script/memory_useage.sh
+++ script/memory_useage.sh
@@ -45,7 +45,7 @@ SWAP_USED_PERCENT=`awk -v x=$SWAP_USED -v y=$SWAP_TOTAL 'BEGIN{printf "%.2f",x *
 
 
 ##create sql file##
-echo "select \'The SQL script execution start at\' ||to_char(sysdate,\'yyyy-mm-dd hh24:mi:ss\') from dual;"
+echo "select 'The SQL script execution start at' ||to_char(sysdate,'yyyy-mm-dd hh24:mi:ss') from dual;"
 echo "insert into xunjian.MEMORY_USEAGE(INSERT_DATE,HOSTNAME,MEMO_TOTAL,MEMO_USED,MEMO_FREE,MEMO_USED_PERCENT,SWAP_TOTAL,SWAP_USED,SWAP_FREE,SWAP_USED_PERCENT)values(to_date('$INSERT_DATE','yyyy-mm-dd hh24:mi:ss'),'$HOSTNAME',$MEMO_TOTAL,$MEMO_USED,$MEMO_FREE,$MEMO_USED_PERCENT,$SWAP_TOTAL,$SWAP_USED,$SWAP_FREE,$SWAP_USED_PERCENT);"
 
 echo "select 'The SQL script MEMORY_USEAGE on the $HOSTNAME has been executed' from dual;" 
